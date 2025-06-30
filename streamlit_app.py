8ca237dca2b4b4b62349a2437aa9e4bfbd859c84
lucasibrahimss <142946220+lucasibrahimss@users.noreply.github.com>
2025-06-30 14:59:18 +0000

    resolvendo problema do import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from .image_processing import ImageProcessor
-from .simulation import run_simulation
-from .streamlit_utils import html_injection
+from image_processing import ImageProcessor
+from simulation import run_simulation
+from streamlit_utils import html_injection
 
 
 def main():
